7834fe30e093e9ee206f7ff70fce9079a15af74e
minkyu <elliotelon@gmail.com>
2023-01-09 10:11:16 +0900

    CustomUserDetailsService 생성

--- src/main/java/com/springboot/blog/entity/Role.java
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -20,10 +20,4 @@ public class Role {
     private Long id;
     private String name;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "users_roles",
-    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-    )
-    private Set<Role> roles;
 }
--- src/main/java/com/springboot/blog/entity/User.java
+++ src/main/java/com/springboot/blog/entity/User.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,4 +26,11 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
